2ca71e14952a279f96f53fb884ad219f2ca1eaef
Rmgriff83 <70349632+Rmgriff83@users.noreply.github.com>
2020-10-03 14:28:20 -0700

    Create key.js

---
+++ src/components/key.js
@@ -0,0 +1,13 @@
+
+
+function getId(len) {
+
+    const chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+    let val = "";
+    for (let i = 0; i < len; ++i) {
+        val += chars[Math.floor(Math.random() * chars.length)]
+    };
+    return val;
+};
+
+export default getId
